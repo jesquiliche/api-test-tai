c04ab33b29eb481a7c27d34aa4fc139f63dab833
Jesús Quintana <jesquiliche@hotmail.comm>
2023-02-24 02:27:03 +0100

    añadiendo controladores

--- app/Http/Controllers/PreguntaController.php
+++ app/Http/Controllers/PreguntaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Pregunta;
 use Illuminate\Http\Request;
 
 class PreguntaController extends Controller
 {
     //
+
+    public function index()
+    {
+        //
+        return Pregunta::all();
+
+    }
 }
